57d40b48b9c68c972dec41a4f21f3a6d4a64bd98
malickcisse <malick.cisse@gambade.co>
2014-06-16 17:08:25 +0200

    hello world working with json response

--- src/com/shuut/servlets/Test.java
+++ src/com/shuut/servlets/Test.java
@@ -46,49 +46,59 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+
 		// KieServices is the factory for all KIE services 
-        KieServices ks = KieServices.Factory.get();
-        
-        // From the kie services, a container is created from the classpath
-        KieContainer kc = ks.getKieClasspathContainer();
-        
-        // From the container, a session is created based on  
-        // its definition and configuration in the META-INF/kmodule.xml file 
-        KieSession ksession = kc.newKieSession("HelloWorldKS");
-        
-        // Once the session is created, the application can interact with it
-        // In this case it is setting a global as defined in the 
-        // org/drools/examples/helloworld/HelloWorld.drl file
-        ksession.setGlobal( "list",
-                            new ArrayList<Object>() );
+		KieServices ks = KieServices.Factory.get();
 
-        // The application can also setup listeners
-        ksession.addEventListener( new DebugAgendaEventListener() );
-        ksession.addEventListener( new DebugRuleRuntimeEventListener() );
+		// From the kie services, a container is created from the classpath
+		KieContainer kc = ks.getKieClasspathContainer();
 
-        // To setup a file based audit logger, uncomment the next line 
-        // KieRuntimeLogger logger = ks.getLoggers().newFileLogger( ksession, "./helloworld" );
-        
-        // To setup a ThreadedFileLogger, so that the audit view reflects events whilst debugging,
-        // uncomment the next line
-        // KieRuntimeLogger logger = ks.getLoggers().newThreadedFileLogger( ksession, "./helloworld", 1000 );
+		// From the container, a session is created based on  
+		// its definition and configuration in the META-INF/kmodule.xml file 
+		KieSession ksession = kc.newKieSession("HelloWorldKS");
 
-        // The application can insert facts into the session
-        final Message message = new Message();
-        message.setMessage( req.getParameter("message") );
-        message.setStatus( Message.HELLO );
-        ksession.insert( message );
+		// Once the session is created, the application can interact with it
+		// In this case it is setting a global as defined in the 
+		// org/drools/examples/helloworld/HelloWorld.drl file
+		ksession.setGlobal( "list",
+				new ArrayList<Object>() );
 
-        // and fire the rules
-        ksession.fireAllRules();
-        
-        // Remove comment if using logging
-        // logger.close();
+		// The application can also setup listeners
+		ksession.addEventListener( new DebugAgendaEventListener() );
+		ksession.addEventListener( new DebugRuleRuntimeEventListener() );
 
-        // and then dispose the session
-        ksession.dispose();
-    }
+		// To setup a file based audit logger, uncomment the next line 
+		// KieRuntimeLogger logger = ks.getLoggers().newFileLogger( ksession, "./helloworld" );
+
+		// To setup a ThreadedFileLogger, so that the audit view reflects events whilst debugging,
+		// uncomment the next line
+		// KieRuntimeLogger logger = ks.getLoggers().newThreadedFileLogger( ksession, "./helloworld", 1000 );
+
+		// The application can insert facts into the session
+		final Message message = new Message();
+		message.setMessage( req.getParameter("message") );
+		String message1 = message.getMessage();
+		message.setStatus( Message.HELLO );
+		ksession.insert( message );
+
+		// and fire the rules
+		ksession.fireAllRules();
+
+		// Remove comment if using logging
+		// logger.close();
+
+		// and then dispose the session
+		resp.setContentType("application/json");
+		
+		String jsonObject = "{ first_message: "+message1+", edited_message: "+message.getMessage()+" }";
+		
+		// Get the printwriter object from response to write the required json object to the output stream      
+		PrintWriter out = resp.getWriter();
+		// Assuming your json object is **jsonObject**, perform the following, it will return your json object  
+		out.print(jsonObject);
+		out.flush();
+		ksession.dispose();
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
